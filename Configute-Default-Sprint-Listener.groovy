18c822334333c7dd90ec6a46b16c2d536a41b62e
Shachar297 <thisishachar@gmail.com>
2021-10-20 12:13:11 +0300

    Added a listener for sprint default configuration.

---
+++ Configute-Default-Sprint-Listener.groovy
@@ -0,0 +1,52 @@
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.MutableIssue;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.CustomFieldManager;
+import com.onresolve.jira.groovy.user.FieldBehaviours;
+import com.onresolve.jira.groovy.user.FormField;
+import groovy.transform.BaseScript;
+import com.atlassian.greenhopper.service.rapid.view.RapidViewService
+import com.atlassian.greenhopper.service.sprint.SprintIssueService
+import com.atlassian.greenhopper.service.sprint.SprintManager
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.onresolve.scriptrunner.runner.customisers.JiraAgileBean
+import com.onresolve.scriptrunner.runner.customisers.WithPlugin;
+import com.atlassian.jira.event.type.EventDispatchOption;
+import com.atlassian.jira.issue.fields.CustomField;
+
+@BaseScript FieldBehaviours fieldBehaviours;
+@WithPlugin("com.pyxis.greenhopper.jira")
+@JiraAgileBean
+RapidViewService rapidViewService
+@JiraAgileBean
+SprintIssueService sprintIssueService
+@JiraAgileBean
+SprintManager sprintManager
+//     - Managers -
+def customFieldManager = ComponentAccessor.getCustomFieldManager();
+def loggedInUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+//    - Sprint Related -
+Issue issue = event.issue;
+
+def sprintsToIssue = [] as Collection;
+
+def boardId = 287;
+def view = rapidViewService.getRapidView(loggedInUser, boardId).getValue()
+def sprintField = customFieldManager.getCustomFieldObject("customfield_10104");
+def sprints = sprintManager.getSprintsForView(view).getValue()
+def activeSprint = sprints.find { it.active }
+sprintsToIssue.add(activeSprint)
+
+if (activeSprint && !issue.getCustomFieldValue(sprintField)) {
+	handleIssueChanges(issue, activeSprint.id, sprintsToIssue, sprintField);
+}
+
+private void handleIssueChanges (Issue issue, Long sprintId, Collection issueSprints, CustomField sprintField) {
+	def loggedInUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+	def issueManager = ComponentAccessor.getIssueManager();
+    
+    MutableIssue currentIssue = issueManager.getIssueObject(issue.key.toString());
+	currentIssue.setCustomFieldValue(sprintField, issueSprints);
+    issueManager.updateIssue(loggedInUser, currentIssue, EventDispatchOption.DO_NOT_DISPATCH, false);
+}
